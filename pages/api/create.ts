7bdeba64f8aeb069d532719b124f6ac2028c6223
Luis Dourado <lfaddourado@gmail.com>
2022-09-15 08:59:37 -0300

    Created note handler

---
+++ pages/api/create.ts
@@ -0,0 +1,21 @@
+import { prisma } from '../../lib/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { title, content } = req.body;
+
+  try {
+    await prisma.note.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    res.status(200).json({ message: 'Nota Criada' });
+  } catch (error) {
+    console.log('Falha ao criar nota');
+  }
+}
