eb623a5e358457a83ad1ac59db660d38b2f13ea2
Luis Dourado <lfaddourado@gmail.com>
2022-09-16 10:05:54 -0300

    add update and delete function & daisyui styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { prisma } from '../lib/prisma';
+
+interface Notes {
+  notes: {
+    id: string;
+    title: string;
+    content: string;
+  }[];
+}
 
 interface FormData {
   title: string;
@@ -7,12 +17,17 @@ interface FormData {
   id: string;
 }
 
-const Home: NextPage = () => {
+const Home = ({ notes }: Notes) => {
   const [form, setForm] = useState<FormData>({
     title: '',
     content: '',
     id: '',
   });
+  const router = useRouter();
+
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
 
   async function create(data: FormData) {
     try {
@@ -22,7 +37,31 @@ const Home: NextPage = () => {
           'Content-Type': 'application/json',
         },
         method: 'POST',
-      }).then(() => setForm({ title: '', content: '', id: '' }));
+      }).then(() => {
+        if (data.id) {
+          deleteNote(data.id);
+          setForm({ title: '', content: '', id: '' });
+          refreshData();
+        } else {
+          setForm({ title: '', content: '', id: '' });
+          refreshData();
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function deleteNote(id: string) {
+    try {
+      fetch(`http://localhost:3000/api/note/${id}`, {
+        headers: {
+          'Content-type': 'application/json',
+        },
+        method: 'DELETE',
+      }).then(() => {
+        refreshData();
+      });
     } catch (error) {
       console.log(error);
     }
@@ -38,34 +77,128 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      <h1 className="text-center font-bold text-2xl mt-4">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit(form);
-          }}
-          className="w-auto min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch"
-        >
-          <input
-            type="text"
-            placeholder="Title"
-            value={form.title}
-            onChange={(e) => setForm({ ...form, title: e.target.value })}
-            className="border-2 rounded border-gray-600 p-1"
-          />
-          <textarea
-            placeholder="Content"
-            value={form.content}
-            onChange={(e) => setForm({ ...form, content: e.target.value })}
-            className="border-2 rounded border-gray-600 p-1"
-          />
-          <button type="submit" className="bg-blue-500 text-white rounded p-1">
-            Adicionar +
-          </button>
-        </form>
-      </h1>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">lunoTE</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <a href="https://github.com/Lutefd">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/luis-dourado-09722a212/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <h1 className="text-5xl font-bold text-center mt-5 mb-10"> Notas</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit(form);
+        }}
+        className="w-auto min-w-[30%] max-w-min mx-auto space-y-4 flex flex-col form-controls"
+      >
+        <input
+          type="text"
+          placeholder="Title"
+          value={form.title}
+          onChange={(e) => setForm({ ...form, title: e.target.value })}
+          className="input input-bordered input-primary w-full max-w-s"
+        />
+        <textarea
+          placeholder="Content"
+          value={form.content}
+          onChange={(e) => setForm({ ...form, content: e.target.value })}
+          className="textarea textarea-primary"
+        />
+        <button type="submit" className="btn btn-primary">
+          Adicionar +
+        </button>
+      </form>
+      <div className="w-auto min-w-[40%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch">
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id} className="border rounded-xl border-primary p-2">
+              <div className="flex justify-between">
+                <div className="flex-1">
+                  <h3 className="font-bold">{note.title}</h3>
+                  <p className="text-sm">{note.content}</p>
+                </div>
+                <button
+                  onClick={() =>
+                    setForm({
+                      title: note.title,
+                      content: note.content,
+                      id: note.id,
+                    })
+                  }
+                  className="btn btn-outline btn-primary"
+                >
+                  editar
+                </button>
+                <div className="p-1"></div>
+                <button
+                  onClick={() => deleteNote(note.id)}
+                  className="btn btn-outline btn-error px-3"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const notes = await prisma.note.findMany({
+    select: {
+      title: true,
+      id: true,
+      content: true,
+    },
+  });
+
+  return {
+    props: {
+      notes,
+    },
+  };
+};
